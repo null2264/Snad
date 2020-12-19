d53e23052ea8bfc31acb688a6355fd60aedccd91
Ahmad Ansori Palembani <46041660+null2264@users.noreply.github.com>
2020-12-19 10:58:03 +0700

    Import stuff

--- src/main/java/com/snad/mixin/SugarCaneMixin.java
+++ src/main/java/com/snad/mixin/SugarCaneMixin.java
@@ -4,11 +4,11 @@ import com.snad.registry.ModTags;
 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
 import net.minecraft.block.SugarCaneBlock;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.WorldView;
